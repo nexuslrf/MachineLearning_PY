eb3b0dcb574410d890201a7cc18a262e4ff89cd5
RF Liang <shootingstarlrf@hotmail.com>
2018-01-22 21:26:24 +0800

    ~

--- datasets&models.py
+++ datasets&models.py
@@ -11,6 +11,11 @@ data_y = loaded_data.target
 model = LinearRegression()
 model.fit(data_X,data_y)
 
+print model.coef_ # linear parameters
+print model.intercept_ # interception
+print model.get_params() # model's init func's init parameters
+print model.score(data_X,data_y) # R^2 coefficient of determination in LinearRegression: Show how well the learning is by percentage
+
 print model.predict(data_X[:4,:])
 print data_y[:4]
 
