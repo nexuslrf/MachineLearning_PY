51493dd9afc481c946a2ba05d6fa8f65034ef56a
RF Liang <shootingstarlrf@hotmail.com>
2018-02-18 10:41:33 +0800

    ~

--- jupyter_notes/Q_learning_1D_EX.py
+++ jupyter_notes/Q_learning_1D_EX.py
@@ -18,6 +18,71 @@ def build_q_table(n_state, actions):
 	return table
 
 def choose_action(state, q_table):
-	
+	#This is how to choose an action
+    state_actions = q_table.iloc[state,:]
+    if (np.random.uniform()> EPSILON) or ((state_actions == 0).all()): # act non-greedy or state-action have no value
+        action_name = np.random.choice(ACTIONS)
+    else: # act greedy
+        action_name = state_actions.idxmax()
+         # replace argmax to idxmax as argmax means a different function in newer version of pandas
+    return action_name
 
-build_q_table(N_STATES,ACTIONS)
+def get_env_feedback(S,A):
+    # This is how agent will interact with the environment
+    if A == 'right':  # move right
+        if S == N_STATES - 2: #terminate
+            S_ = 'terminal'
+            R = 1       
+        else:
+            S_ = S+1
+            R = 0
+    else: # move left
+        R = 0
+        if S == 0:
+            S_ = S # reach the wall
+        else:
+            S_ = S - 1
+    return S_, R
+
+def update_env(S,episode,step_counter):
+    env_list = ['-']*(N_STATES-1)+['T'] # '---------T'
+    if S == 'terminal':
+        interaction = 'Episode %s: total_steps = %s' % (episode+1, step_counter)
+        print('\r{}'.format(interaction),end='')
+        time.sleep(2)
+        print('\r                                ', end='')
+    else:
+        env_list[S] = 'o'
+        interaction = ''.join(env_list)
+        print('\r{}'.format(interaction),end='')
+        time.sleep(FRESH_TIME)
+
+def r1():
+    # main part of RL loop
+    q_table = build_q_table(N_STATES,ACTIONS)
+    for episode in range(MAX_EPISODES):
+        step_counter = 0
+        S = 0
+        is_terminated = False
+        update_env(S,episode,step_counter)
+        while not is_terminated:
+            A = choose_action(S,q_table)
+            S_, R = get_env_feedback(S,A)
+            q_predict = q_table.loc[S,A]
+            if S_ != 'terminal':
+                q_target = R + GAMMA* q_table.iloc[S_,:].max() # next state is not terminal
+            else:
+                q_target = R # next state is terminal
+                is_terminated = True
+
+            q_table.loc[S,A]+=ALPHA*(q_target- q_predict) # update
+            S = S_ #move to next state
+
+            update_env(S,episode,step_counter+1)
+            step_counter +=1
+    return q_table
+
+if __name__ == '__main__':
+    q_table = r1()
+    print('\r\nQ_table:\n')
+    print(q_table)
