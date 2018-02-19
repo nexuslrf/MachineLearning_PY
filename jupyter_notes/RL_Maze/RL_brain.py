53b24a95170c64c7fda626b8221a51ed60e05459
RF Liang <shootingstarlrf@hotmail.com>
2018-02-19 16:47:50 +0800

    ~

--- jupyter_notes/RL_Maze/RL_brain.py
+++ jupyter_notes/RL_Maze/RL_brain.py
@@ -1,34 +1,14 @@
 import numpy as np
 import pandas as pd
 
-class QLearningTable(object):
-    """docstring for QLearningTa"""
-    def __init__(self, actions, learning_rate=0.01, reward_decay=0.9,e_greedy=0.8):
+class RL(object):
+    def __init__(self, actions,learning_rate=0.01,reward_decay=0.9,e_greedy=0.8):
         self.actions = actions # a list
         self.lr = learning_rate
         self.gamma = reward_decay
         self.epsilon = e_greedy
+
         self.q_table = pd.DataFrame(columns = self.actions, dtype=np.float64)
-
-    def choose_action(self, observation):
-        self.check_state_exist(observation)
-        # select an action
-        if np.random.uniform()<self.epsilon:
-            state_action = self.q_table.loc[observation,:]
-            state_action = state_action.reindex(np.random.permutation(state_action.index)) # some actions have the same value to make them disordered 
-            action=state_action.idxmax()
-        else:
-            action = np.random.choice(self.actions)
-        return action
-
-    def learn(self, s,a,r,s_):
-        self.check_state_exist(s_)
-        q_predict = self.q_table.loc[s,a]
-        if s_ != 'terminal':
-            q_target = r + self.gamma*self.q_table.loc[s_,:].max()
-        else:
-            q_target = r
-        self.q_table.loc[s,a] += self.lr * (q_target - q_predict)
 
     def check_state_exist(self, state):
         if state not in self.q_table.index:
@@ -41,4 +21,69 @@ class QLearningTable(object):
                     )
                 )
 
-        
+    def choose_action(self, observation):
+        self.check_state_exist(observation)
+        # select an action
+        if np.random.uniform()<self.epsilon:
+            state_action = self.q_table.loc[observation,:]
+            state_action = state_action.reindex(np.random.permutation(state_action.index)) # some actions have the same value to make them disordered 
+            action=state_action.idxmax()
+        else:
+            action = np.random.choice(self.actions)
+        return action
+
+    def learn(self, *args):
+        pass
+
+# off-policy
+class QLearningTable(RL):
+    def __init__(self, actions, learning_rate=0.01, reward_decay=0.9,e_greedy=0.8):
+        super().__init__(actions, learning_rate, reward_decay, e_greedy)
+    
+    def learn(self, s,a,r,s_):
+        self.check_state_exist(s_)
+        q_predict = self.q_table.loc[s,a]
+        if s_ != 'terminal':
+            q_target = r + self.gamma*self.q_table.loc[s_,:].max()
+        else:
+            q_target = r
+        self.q_table.loc[s,a] += self.lr * (q_target - q_predict)
+
+# on-policy
+class SarsaTable(RL):
+    def __init__(self, actions, learning_rate=0.01, reward_decay=0.9,e_greedy=0.8):
+        super().__init__(actions, learning_rate, reward_decay, e_greedy)
+
+    def learn(self, s,a,r,s_,a_): # add a_ to the learn compared with Qlearning
+        self.check_state_exist(s_)
+        q_predict = self.q_table.loc[s,a]
+        if s_ != 'terminal':
+            q_target = r + self.gamma*self.q_table.loc[s_,a_]
+        else:
+            q_target = r
+        self.q_table.loc[s,a] += self.lr * (q_target - q_predict)
+
+class SarsaLambdaTable(RL):
+    # here trace_decay represent lambda
+    def __init__(self, actions, learning_rate=0.01, reward_decay=0.9,e_greedy=0.8,trace_decay=0.9):
+        super().__init__(actions, learning_rate, reward_decay, e_greedy)
+
+         # 后向观测算法, eligibility trace.
+        self.lambda_ = trace_decay
+        self.eligibility_trace = self.q_table.copy()    # 空的 eligibility trace 表
+
+    #check_state_exist 和之前的是高度相似的. 唯一不同的地方是我们考虑了 eligibility_trace,
+    def check_state_exist(self, state):
+        if state not in self.q_table.index:
+            # append new state to q table
+            to_be_append = pd.Series(
+                    [0] * len(self.actions),
+                    index=self.q_table.columns,
+                    name=state,
+                )
+            self.q_table = self.q_table.append(to_be_append)
+
+            # also update eligibility trace
+            self.eligibility_trace = self.eligibility_trace.append(to_be_append)
+
+    
---
+++ jupyter_notes/RL_Maze/Sarsa.py
@@ -0,0 +1,48 @@
+from maze_env import Maze
+from RL_brain import SarsaTable
+
+def update():
+    cnt_success = 0
+    cnt_failure = 0
+    for episode in range(50):
+        # reset environment
+        observation = env.reset()
+
+        # RL choose action based on observation 
+        # Differ from Qlearning
+        action = RL.choose_action(str(observation))
+
+        while True:
+            env.render()
+            #  # RL take action and get next observation and reward
+            observation_, reward, done = env.step(action)
+
+            # RL choose action based on next observation
+            action_ = RL.choose_action(str(observation_))
+
+            # RL learn from this transition (s, a, r, s, a) ==> Sarsa
+            RL.learn(str(observation), action, reward, str(observation_), action_)
+
+            # swap
+            observation = observation_
+            action = action_
+
+            if done:
+                if reward == 1:
+                    cnt_success += 1
+                elif reward == -1:
+                    cnt_failure +=1
+                break
+
+    print("game over")
+    print("success: %d\nfailure: %d"%(cnt_success,cnt_failure))
+    print(RL.q_table)
+    env.destroy()
+
+
+if __name__ == "__main__":
+    env = Maze()
+    RL = SarsaTable(actions = list(range(env.n_actions)),e_greedy=0.9)
+
+    env.after(50,update)
+    env.mainloop()
